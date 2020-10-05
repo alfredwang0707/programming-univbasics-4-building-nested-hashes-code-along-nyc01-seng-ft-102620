f118bc9ede03db6a3ea88f53c09d8e73130d5cf4
Alfred Wang <alfredwang0707@gmail.com>
2020-10-05 18:08:19 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -24,8 +24,7 @@ epic_tragedy = {
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliet", age: "15", 
-      status: "alive"},
+      :heroine => {name: "Juliet", age: "15",status:"alive"},
       :heroine_friends => [
           {name: "Steven",
           age: "30",
